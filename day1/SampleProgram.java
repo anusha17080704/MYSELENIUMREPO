9b580aecab7d0e7ce36b5e02448aacb0ee24523a
ANUSHA <149173985+anusha17080704@users.noreply.github.com>
2023-10-27 19:15:27 +0530

    Add files via upload
    WEEK 1

---
+++ day1/SampleProgram.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+public class SampleProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome to testing");
+	}
+
+}
---
+++ day2/Firefox.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float BrowserVersion = 100.2f;
+		String BrowserName = "Firefox";
+		boolean isVisible =true;
+		int releaseyear = 1998;
+		char browserLogo ='f';
+		System.out.println("BrowserVersion is "+BrowserVersion);
+		System.out.println("BrowserName is " +BrowserName);
+		System.out.println("isVisible is " +true);
+		System.out.println("releaseyear is " +1998);
+		System.out.println("browserLogo is "+browserLogo);
+	}
+
+}
+
