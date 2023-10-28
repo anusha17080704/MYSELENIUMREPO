d8120273701a45d7fc9b3489512d421ec9de04bf
ANUSHA <149173985+anusha17080704@users.noreply.github.com>
2023-10-28 21:38:09 +0530

    Add files via upload

---
+++ Report.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Report {
+
+	public static void main(String[] args) {
+		
+		Student Details = new Student();
+		System.out.println("Student Name is : " + Details.StudentName);
+		System.out.println("Roll no is : " + Details.RollNo);
+		System.out.println("College Name is : " + Details.CollegeName);
+		System.out.println("Marks scored is : " + Details.MarkScored);
+		System.out.println("CGPA is : " + Details.Cgpa);
+
+	}
+
+}
