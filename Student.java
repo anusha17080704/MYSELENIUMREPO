dbdb8897178df53a628bee48a40c2e7731d0932b
ANUSHA <149173985+anusha17080704@users.noreply.github.com>
2023-10-28 21:37:35 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Student {
+
+	String StudentName = "Anusha";
+	int RollNo = 7;
+	String CollegeName = "Harvard";
+	int MarkScored = 100;
+	float Cgpa = 9.5f;
+	
+	
+	}
+	
+
+
+
