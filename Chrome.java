0f7d0d94e477855fc81c358a3bdba1245ff330b1
ANUSHA <149173985+anusha17080704@users.noreply.github.com>
2023-10-28 20:39:26 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Chrome {
+
+	public void getName() {
+	
+		String name = "This is google chrome";
+		System.out.println(name);
+	}
+	public void PrintName()
+	{
+	String name = "This is google chrome";
+	System.out.println(name);
+
+	}
+public static void main(String args[])
+{
+	Chrome obj = new Chrome();
+	obj.getName();
+	obj.PrintName();
+}
+}
