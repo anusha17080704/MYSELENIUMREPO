c109a5ea54edb7868bead03a549ceff1a0ec0160
ANUSHA <149173985+anusha17080704@users.noreply.github.com>
2023-10-28 21:05:35 +0530

    Add files via upload

---
+++ Bike.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Bike {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car obj = new Car();
+		obj.applyBreak();
+		obj.soundHorn();
+
+}
+
+}
