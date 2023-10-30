285a714338a3467ff43dcc0b06525166243805af
ANUSHA <149173985+anusha17080704@users.noreply.github.com>
2023-10-30 22:29:57 +0530

    week2

---
+++ FindOddNumbers.java
@@ -0,0 +1,17 @@
+package week2.day1;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args) {
+		
+// TODO Auto-generated method stub
+System.out.println("List of Odd Numbers: ");
+for(int i =1;i<=10;i++)
+	if(i%2==1)
+	{
+		System.out.println(i);
+	}
+	}
+	
+
+}
